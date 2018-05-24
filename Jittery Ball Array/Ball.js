3ac088dbd5de6de7e33aa55ccf72acb37b8e6803
iamcharliecohen <iamcharliecohen@gmail.com>
2018-05-24 13:35:36 -0400

    patriotic

--- Jittery Ball Array/Ball.js
+++ Jittery Ball Array/Ball.js
@@ -1,17 +1,19 @@
 function Ball(d, a){
     this.d = d;
-    this.a = a;
+    this.a = a*PI/180;
     this.originX = width/2;
     this.originY = height/2;
-    this.r = map(this.d,0,width,3,20);
-    this.x = this.originX + this.d * cos(this.a);
-    this.y = this.originY + this.d * sin(this.a);
+    this.r = map(this.d,0,width,3,40);
+    //this.r = 10;
+    this.x = this.originX + this.d * sin(this.a)/2;
+    this.y = this.originY + this.d * cos(this.a)/2;
+    this.num = 0;
 
     
     //this.x = random(this.r, width-this.r);
     //this.y = random(this.r, height-this.r);
-    //this.col = (random(255),random(255),random(255));
-    this.col = (255,255,255);
+    this.col = color(255, 204, 0);
+    
 
     this.create = function(){
         fill(this.col);
@@ -30,8 +32,34 @@ function Ball(d, a){
         }
     }
 
+    this.quadrant = function(){
+        if(this.num % 9 === 0){
+            this.col = color('red');   
+        } else if(this.num % 9 === 1){
+            this.col = color('white');   
+        } else if(this.num % 9 === 2){
+            this.col = color('blue');   
+        } else if(this.num % 9 === 3){
+            this.col = color('red');   
+        } else if(this.num % 9 === 4){
+            this.col = color('white');   
+        } else if(this.num % 9 === 5){
+            this.col = color('blue');   
+        } else if(this.num % 9 === 6){
+            this.col = color('red');   
+        } else if(this.num % 9 === 7){
+            this.col = color('white');   
+        } else if(this.num % 9 === 8){
+            this.col = color('blue');   
+        }
+    }
+
     this.update = function(){     
-        this.a += map(this.d, 0,width/5,0.1,0);
+        this.a += map(this.d, 0,width/2,6*PI/180,0.001*PI/180); 
+        this.num = floor(this.a / (PI/9));
+        this.quadrant();
+        fill(this.col);
+         
         
         this.x = this.originX + this.d * cos(this.a);
         this.y = this.originY + this.d * sin(this.a);
